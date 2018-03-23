942df706a82f6cfeb77a44c6565f24efb4dba40e
editbit <as99346@gmail.com>
2018-03-24 06:05:40 +0900

    Add files via upload

--- FeedTheCat.cpp
+++ FeedTheCat.cpp
@@ -3,22 +3,23 @@
 using namespace std;
 
 //A. Feed the cat(http://codeforces.com/contest/955/problem/A)
-//Wrong answer on test 8
 int main() {
-	int hh, mm, h, h2, d, c, n;
-	float a, a2;
+	double hh, mm, h, h2, d, c, n;
+	double a, a2;
 
 	cin >> hh >> mm >> h >> d >> c >> n;
 
-	a = ceil(h / (float)n) * c;
+	a = ceil(h /n) * c;
 	if (hh < 20) {
 		h2 = h+ ((20 - hh) * 60 - mm)*d;
-		a2 = ceil(h2 / (float)n) * c*0.8;
+		a2 = ceil(h2 / n) * c*0.8;
 		a= (a < a2) ? a : a2;
 	}
+	else if (hh >= 20) {
+		a = a * 0.8;
+	}
+	cout.precision(4);
+	cout << fixed << a;
 
-	cout << a;
-
-	system("pause");
 	return 0;
 }
