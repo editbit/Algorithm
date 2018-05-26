6289cce1753bf138227c4cba449df71750c6b846
editbit <as99346@gmail.com>
2018-05-27 01:40:48 +0900

    Create MySort.h

---
+++ sort/MySort.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class MySort {
+	private:
+		static void swap(int &a, int&b);
+
+		static void merge(int *s, int l, int m, int h);	// mergesort
+		static int partition(int *s, int l, int h);		// quicksort
+
+	public:
+		static void mergesort(int *s, int l, int h);	// O(n log n) O(n log n) O(n log n)
+		static void quicksort(int *s, int l, int h);	// O(n log n) O(n log n) O(n^2)
+};
